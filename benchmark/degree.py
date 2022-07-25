5ce78593306f9f7aef0786757a7cd6f49566c28d
LLipter <llipter@foxmail.com>
2022-07-25 14:15:51 +0200

    update a figrue

--- benchmark/degree.py
+++ benchmark/degree.py
@@ -41,6 +41,7 @@ if __name__ == '__main__':
     plt.title("Intel® Core™ i7-7700HQ CPU @ 2.80GHz (Kabylake)\nL1: 128KB, L2: 256KB, L3: 6MB\nCode Length=128", loc='left', fontsize=16, fontweight=1, color='black')
     plt.xlabel("Relevant Distance", fontsize=16)
     plt.ylabel("Runtime[ms]", fontsize=16)
-    # plt.savefig('timing_add_opt.pdf')
 
-    plt.show()
+    plt.savefig('degree.pdf')
+
+    # plt.show()
